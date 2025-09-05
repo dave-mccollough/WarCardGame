b52eac7e268184bc340af50298b4f381967b1925
Dave McCollough <davemccollough@gmail.com>
2025-09-05 10:25:36 -0600

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Dave McCollough on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
